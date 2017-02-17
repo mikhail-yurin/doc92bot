65687534940f430ce5c02019862896c04d71fcca
Michael <mixa.yurin@yandex.ru>
2017-02-17 23:30:57 +0300

    small improvements

--- doc92_bot.js
+++ doc92_bot.js
@@ -12,13 +12,23 @@ cfg.getProps()
 
         var bot = new TelegramBot(props.token.toString(), { polling: true });
         var checker = new Checker();
+        var commands = [
+            '/start',
+            '/help',
+            '/stop',
+            '/feedback'
+        ];
 
         bot.onText(/\/start/g, function (msg) {
             bot.sendMessage(msg.chat.id, 'Здравствуйте!\nЯ создан, чтобы следить за наличием талонов на прием к врачу на сервисе https://doctor-92.ru\nЧтобы посмотреть инструкцию, пришлите "/help"');
         });
 
         bot.onText(/\/help/g, function (msg) {
-            bot.sendMessage(msg.chat.id, '1. В своем браузере откройте ссылку с выбором специалиста и времени посещения\n2. Скопируйте адрес ссылки и пришлите её мне\n\nЯ буду периодически проверять наличие талонов на прием (на два месяца вперед) и в случае появления, сообщу вам об их наличии\nЧтобы отменить проверку расписания, пришлите мне "/stop"');
+            bot.sendMessage(msg.chat.id, '1. В своем браузере откройте ссылку с выбором специалиста и времени посещения.\n2. Скопируйте адрес ссылки и пришлите её мне.\n\nЯ буду периодически проверять наличие талонов на прием (на два месяца вперед) и в случае появления, сообщу вам об их наличии.\nЧтобы отменить проверку расписания, пришлите мне "/stop"');
+        });
+
+        bot.onText(/\/feedback/g, function (msg) {
+            bot.sendMessage(msg.chat.id, 'Бот не сохраняет никаких ваших личных данных.\nИсходный код доступен на https://github.com/mikhail-yurin/doc92bot.\nПредложение по улучшению работы бота вы можете оставить тут https://github.com/mikhail-yurin/doc92bot/issues/new\nили отправив письмо на mixa.yurin@yandex.ru.');
         });
 
         bot.onText(/\/stop/g, function (msg) {
@@ -32,7 +42,7 @@ cfg.getProps()
         });
 
         bot.on('message', function (msg) {
-            if (msg.text != '/start' && msg.text != '/help' && msg.text != '/stop') {
+            if (commands.indexOf(msg.text) == -1) {
                 if (/https\:\/\/doctor\-92\.ru\/record\/\?/g.test(msg.text)) {
                     bot.sendMessage(msg.chat.id, "Принято! Буду держать вас в курсе наличия талонов!");
                     fs.writeFile(os.tmpdir() + path.sep + 'doc92' + path.sep + `${msg.from.id}`, msg.text, (err) => {
